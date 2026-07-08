aedf543aa2c1d841c09c118a62cb44509a627ca4
ARUNEISEN <aruneisen@gmail.com>
2026-07-08 13:26:12 +0530

    Testing branch file changed

--- tests/Git_pratice-demo-one.js
+++ tests/Git_pratice-demo-one.js
@@ -1,4 +1,4 @@
 console.log('i have modified this txt file to js file');
 console.log('I am practicing Git.');
-console.log('I am practice git stash here now');
+console.log('This line I changed from testing branch!.');
 
