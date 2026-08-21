58e991825c84d8ad28e4b9d9db1a27c397badaf2
ARUNEISEN <aruneisen@gmail.com>
2026-08-21 17:27:42 +0530

    fb code added

--- tests/Amazon.spec.js
+++ tests/Amazon.spec.js
@@ -3,6 +3,6 @@ import {test,expect} from "@playwright/test";
 test("practicing the trace viewer", async ({page}) => {
 
     await page.goto("https://www.facebook.com/");
-    await page.waitForTimeout(7000);
+    await page.waitForTimeout(2000);
     
 })
